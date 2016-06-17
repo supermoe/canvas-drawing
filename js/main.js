c1509c0c1a90c2de148aeccda7094a78ad9197c5
supermoe <gdmaxis@gmail.com>
2016-06-17 22:26:24 +0200

    double canvas system.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 //conf
 var refreshRate = 30;   // Polling rate during a stroke
 var color = '#E62062';	// Stroke color
-var simplify = true;	// Whether to simplify drawn strokes or not
 var weight = 3;		// Stroke weight/width
+var debug = true;	// Display debug
 
 // Entry point
-function main(canvas){	
+function main(c_draw, c_display){	
 	// Init
 	var drawId;
 	var lastPosition = {x:0, y:0};
@@ -13,17 +13,18 @@ function main(canvas){
 	var paths = [];
 	var currentPath;
 
-
+	c_display.width = c_draw.width;
+	c_display.height = c_draw.height;
 	
 	// Update mouse position
-	$(canvas).mousemove(e => {
-		var rect = canvas.getBoundingClientRect();
+	$(c_draw).mousemove(e => {
+		var rect = c_draw.getBoundingClientRect();
 		mouse.x = e.pageX - rect.left;
 		mouse.y = e.pageY - rect.top;
 	});
 	
 	// Start a stroke
-	$(canvas).mousedown(function (e) {
+	$(c_draw).mousedown(function (e) {
 		currentPath = new paper.Path();
 		currentPath.strokeColor = color;
 		currentPath.strokeWidth = weight;
@@ -36,14 +37,16 @@ function main(canvas){
 	});
 
 	// End a stroke and save it
-	$(canvas).mouseup(() => {
-		if (simplify) currentPath.simplify();
+	$(c_draw).mouseup(() => {
+		() => {
+			currentPath.simplify();	
+			c_display.getContext('2d').drawImage(c_draw, 0, 0);
+		}();
 		paths.push(currentPath);
 		clearInterval(drawId);
 	});
 
 }
-
 // Draw a point
 function draw(mouse, path){
 	path.add(mouse.x, mouse.y);
